19e1c6851191b0ed618e36b2194a033aaa66668d
agraham <agraham0925@gmail.com>
2018-04-20 16:04:41 -0500

    can view single video image on add ejs page

--- public/js/api.js
+++ public/js/api.js
@@ -15,6 +15,7 @@ $('#button').on('click', (event) => {
 			key: "AIzaSyC_xdKtskgVo7e_iNelQcq3Ti6BkeDy3Sw",
 			q: keyword,
 			type: "video",
+			videoEmbeddable: true,
 			part: "snippet"
 		},
 		type: "GET",
@@ -24,13 +25,22 @@ $('#button').on('click', (event) => {
 			console.log("didn't work")
 		}
 	})
-
-	console.log('button was clicked')
 })
 
 
 function getVideos(data) {
-	console.log(data)
+	console.log(data);
 
-	console.log(data.items[0].id.videoId)
+	console.log(data.items[2].snippet.thumbnails.high.url);
+	console.log(data.items[2].snippet.title);
+
+	for(let i = 0; i < data.items.length; i++) {
+		console.log(data.items[i])
+	}
+
+	const thumbnailURL = data.items[2].snippet.thumbnails.medium.url;
+	const thumbnailImage = $('<img>').attr('src', thumbnailURL)
+	// const thumbnailImage = $('<img src= thumbnailURL />')
+
+	$('#theDiv').append(thumbnailImage)
 }
