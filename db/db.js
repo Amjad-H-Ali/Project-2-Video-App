cb8b4bcbac33905c802f0e00159fee2a614104fc
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-04-20 09:17:15 -0500

    Setup DB and a connection with mongoose

---
+++ db/db.js
@@ -0,0 +1,21 @@
+//Require mongoose to connect schema with our DB
+const mongoose = require('mongoose');
+//Defining variable to connect mongoose with MongoDB and naming DB
+connectionString = 'mongodb/localhost/videoApp';
+//Connected mongoose to our DB
+mongoose.connect(connectionString);
+
+
+//Call Backs
+
+mongoose.connection.on('connected', ()=>{
+	console.log('Connected to MongoDB...');
+});
+
+mongoose.connection.on('disconnected', ()=>{
+	console.log('Disconnected from MongoDB');
+});
+
+mongoose.connection.on('error', (err)=>{
+	console.log(`Error connecting to MongoDB: ------${err}------`);
+})
--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const app = express();
 const PORT = 3000;
 
 
+//Requiring our DB
+require('./db/db');
+
+
+
 
 
 
