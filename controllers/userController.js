4f24a41d3ef1dbc41eaf489c1d14ab6d20727f3d
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-04-20 11:24:19 -0500

    Created get routes to show, liked, add, edit, index, login pages

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,20 +6,52 @@ const router = express.Router();
 const User = require('../models/user');
 
 
+//Routes
+
+
 //Get route to render add page
 router.get('/add', (req, res)=>{
 	res.render('user/add.ejs');
-})
+});
+
+
 
 //Get route to render login-signup page
 router.get('/', (req, res)=>{
-	res.render('user/login-signup.ejs')
-})
+	res.render('user/login-signup.ejs');
+});
+
+
 
 //Get route to render index page
 router.get('/index', (req, res)=>{
-	res.render('user/index.ejs')
-})
+	res.render('user/index.ejs');
+});
+
+
+
+//Get route to render show page
+router.get('/show', (req, res)=>{
+	res.render('user/show.ejs');
+});
+
+
+
+//Get route to render edit page
+router.get('/edit', (req, res)=>{
+	res.render('user/edit.ejs');
+});
+
+
+//Get route to render liked-videos page
+router.get('/liked-videos', (req, res)=>{
+	res.render('user/liked-videos');
+});
+
+
+ 
+
+
 
 
 
