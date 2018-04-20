54e00c8535e4aa15eb9a8e89b56cc6f607d5deb3
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-04-20 16:21:50 -0500

    Modified user model page to require data

--- models/user.js
+++ models/user.js
@@ -3,13 +3,30 @@ const mongoose = require('mongoose');
 //Instantiate userSchema with the Schema constructor in mongoose
 const userSchema = new mongoose.Schema({
 
-	firstName: String,
+	firstName:{
+		type: String,
+		unique: false,
+		required: true
+	}, 
 
-	lastName: String,
+	lastName: {
+		type: String,
+		unique: false,
+		required: true
+	},
 
-	userName: String,
+	userName: {
+		type: String,
+		unique: true,
+		required: true
+	},
 
-	password: String
+	password:{
+		type: String,
+		unique: false,
+		required: true
+	}
+
 });
 
 //Export for controllers to use
