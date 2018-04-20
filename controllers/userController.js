7d8594b80f8e867013efc52938fcfd0c7e97cc8f
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-04-20 09:50:11 -0500

    Set up controllers for videos and user

---
+++ controllers/userController.js
@@ -0,0 +1,28 @@
+// require express to use router
+const express = require('express');
+// instantiate router with router constructor
+const router = express.Router();
+//Require models to use schema and db
+const User = require('../models/user');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
+
