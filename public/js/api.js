b02161414d85301a7bca83a8ad0b817ab16c992d
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-04-21 16:12:31 -0500

    Put buttons on each thumbnail

--- public/js/api.js
+++ public/js/api.js
@@ -30,12 +30,21 @@ $('#button').on('click', (event) => {
 
 function getVideos(data) {
 	console.log(data);
-
+	//Iterating through items array of objects in data object
 	for(let i = 0; i < data.items.length; i++) {
-
+		//Make a div that will contain a button and a thumbnail
+		const $div = $('<div>');
+		//Getting url from thumbnails within those objects
 		const thumbnailURL = data.items[i].snippet.thumbnails.medium.url;
-		const thumbnailImage = $('<img>').attr('src', thumbnailURL)
-
-		$('#theDiv').append(thumbnailImage)
+		//Adding url as the src of imgages to make the thumbnails appear
+		const thumbnailImage = $('<img>').attr('src', thumbnailURL);
+		//Append thumbnail to div with a button
+		const btnThumbContain = $('div').append(thumbnailImage).append($('<button>').attr('src','thumbNailBtn').text('share'))
+		// Appending images to div on our view page
+		$(btnThumbContain).appendTo('#thumbNails');
 	}
+
+
+
+	$('#player').attr('src',"http://www.youtube.com/embed/"+data.items[0].id.videoId+"?enablejsapi=1&origin=http://example.com")
 }
