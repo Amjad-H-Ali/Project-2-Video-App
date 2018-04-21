efe2df0f5b48c2887d85116d1ee2f30fbcd9e792
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-04-21 17:23:12 -0500

    Created popup form when sharing video

--- public/js/api.js
+++ public/js/api.js
@@ -32,17 +32,35 @@ function getVideos(data) {
 	console.log(data);
 	//Iterating through items array of objects in data object
 	for(let i = 0; i < data.items.length; i++) {
-		//Make a div that will contain a button and a thumbnail
-		const $div = $('<div>');
+		//Make a div that will contain a button and a thumbnail and set id to specific id of video we are iterating through
+		const $div = $('<div>').attr('id',data.items[i].id.videoId);
 		//Getting url from thumbnails within those objects
 		const thumbnailURL = data.items[i].snippet.thumbnails.medium.url;
 		//Adding url as the src of imgages to make the thumbnails appear
 		const thumbnailImage = $('<img>').attr('src', thumbnailURL);
 		//Append thumbnail to div with a button
-		const btnThumbContain = $div.append(thumbnailImage).append($('<button>').attr('id','thumbNailBtn').text('share'))
+		const btnThumbContain = $div.append(thumbnailImage).append($('<button>').attr('class','thumbNailBtn').text('share'))
 		// Appending images to div on our view page
 		$(btnThumbContain).appendTo('#thumbNails');
 	}
+
+
+	//Event lister on thumbnail button
+	//When clicked, a form will pop up
+	$('.thumbNailBtn').on('click', (event)=>{
+		// When share btn clicked this form will pop up
+		$('.modal').css('display', 'block');
+
+		console.log($(event.target).parent()[0].id);
+	});
+
+	
+
+	//Event listener when pressing the close button on form
+	$('.close').on('click', (e)=>{
+		// close the form
+		$('.modal').css('display', 'none');
+	})
 
 
 
