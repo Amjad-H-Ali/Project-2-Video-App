e526bace904bb7cd8a519757e1a095fd42018f6e
agraham <agraham0925@gmail.com>
2018-04-26 11:21:31 -0500

    user can click on another users name and see list of videos shared by that user

--- controllers/userController.js
+++ controllers/userController.js
@@ -93,10 +93,32 @@ router.get('/show', async(req, res)=>{
 
 
 
+//Get route to render other users' show pages from index page
+router.get('/show/:id', async(req, res)=>{
+
+	//find user by username
+	const userName = req.params.id
+
+	//Find all the videos posted by this user
+	const videos = await Video.find({'user': userName});
+
+	// const videos = Video.find();
+
+	console.log("=================")
+	console.log(userName + " this is userName");
+	console.log("=================")
+	console.log("=================")
+	console.log(videos + " this is videos");
+	console.log("=================")
+
+	//Sending current user to another user's show page
+	res.render('user/otherShow.ejs', {
+
+		videos: videos
+	});
 
 
-
-
+});
 
 
 
