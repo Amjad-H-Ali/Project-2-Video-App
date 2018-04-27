6dbea45071904c2291851ab810dcd95669cb1401
agraham <agraham0925@gmail.com>
2018-04-27 09:53:17 -0500

    updated db name per heroku instructions

--- db/db.js
+++ db/db.js
@@ -1,7 +1,7 @@
 //Require mongoose to connect schema with our DB
 const mongoose = require('mongoose');
 //Defining variable to connect mongoose with MongoDB and naming DB
-connectionString = process.env.DB_HOST;
+connectionString = process.env.MONGODB_URI;
 //Connected mongoose to our DB
 mongoose.connect(connectionString);
 
